c9acb274c94484df05de266d6de705cc76de78ea
Ben Sarrazin <b@srz.io>
2019-01-07 16:44:19 -0500

    An MVC example

--- MVC/MVC/Source/ViewController.swift
+++ MVC/MVC/Source/ViewController.swift
@@ -1,9 +1,75 @@
 import UIKit
 
+enum Pattern {
+    case observable
+    case nonObservable
+}
+
 class ViewController: UIViewController {
     
+    // MARK: - Outlets
+    @IBOutlet private(set) var textField: UITextField!
+    @IBOutlet private(set) var button: UIButton!
+    @IBOutlet private(set) var label: UILabel!
+    
+    // MARK: - Properties
+    var model: Model = .init(value: "Initial Value")
+    private let pattern: Pattern = .nonObservable
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        switch pattern {
+        case .observable: observableViewDidLoad()
+        case .nonObservable: nonObservableViewDidLoad()
+        }
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        switch pattern {
+        case .observable: observableButtonTapped()
+        case .nonObservable: nonObservableButtonTapped()
+        }
+    }
+    
+}
+
+// MARK: - Non Observable Pattern
+extension ViewController {
+    
+    func nonObservableViewDidLoad() {
+        textField.text = model.value
+        label.text = model.value
+    }
+    
+    func nonObservableButtonTapped() {
+        model.value = textField.text ?? ""
+        label.text = model.value
+    }
+    
+}
+
+// MARK: - Observable Pattern
+extension ViewController {
+    
+    func observableViewDidLoad() {
+        textField.text = model.value
+        label.text = model.value
+        
+        NotificationCenter.default
+            .addObserver(
+                forName: Model.valueDidChangeNotification,
+                object: nil,
+                queue: nil,
+                using: { [label] notification in
+                    let value = notification.userInfo?[Model.valueDidChangeUserInfoKey] as? String ?? ""
+                    label?.text = value
+                })
+    }
+    
+    func observableButtonTapped() {
+        model.value = textField.text ?? ""
     }
     
 }
--- MVC/MVCTests/Source/MVCTests.swift
+++ MVC/MVCTests/Source/MVCTests.swift
@@ -1,14 +1,22 @@
-@testable import MVVM
+@testable import MVC
 import XCTest
 
 class MVCTests: XCTestCase {
     
     func testExample() {
-    }
-    
-    func testPerformanceExample() {
-        self.measure {
-        }
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = storyboard.instantiateInitialViewController() as! ViewController
+        viewController.model = Model(value: "test value")
+        viewController.beginAppearanceTransition(true, animated: false)
+        
+        XCTAssertTrue(viewController.label.text == "test value")
+        XCTAssertTrue(viewController.textField.text == "test value")
+        
+        viewController.textField.text = "new test value"
+        viewController.button.sendActions(for: .touchUpInside)
+        
+        XCTAssertTrue(viewController.label.text == "new test value")
+        XCTAssertTrue(viewController.textField.text == "new test value")
     }
     
 }
